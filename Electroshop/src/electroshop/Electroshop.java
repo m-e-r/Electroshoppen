159d61385d76244b0853d19581d7f9325735fa43
JacobSogaard <jacos16@student.sdu.dk>
2017-04-19 10:49:10 +0200

    update

--- Electroshop/src/electroshop/Electroshop.java
+++ Electroshop/src/electroshop/Electroshop.java
@@ -22,7 +22,7 @@ public class Electroshop extends Application {
     @Override
     public void start(Stage primaryStage) {
         Button btn = new Button();
-        btn.setText("Say 'Hello World'");
+        btn.setText("HELLO WORLD!!!!!");
         btn.setOnAction(new EventHandler<ActionEvent>() {
             
             @Override
