652824615ee553348821fbd5ce701d7038b90f7c
Zunac <tobias_dahl@yahoo.dk>
2017-05-16 14:43:07 +0200

    Javadoc and changed the fillProducts method (Needs testing)

--- ElectroshoppenProjekt/src/ProductStuff/ProductCatalog.java
+++ ElectroshoppenProjekt/src/ProductStuff/ProductCatalog.java
@@ -9,6 +9,7 @@ import DBManager.DBConnection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.TreeMap;
 import javafx.beans.Observable;
 import javafx.collections.FXCollections;
@@ -33,22 +34,27 @@ public class ProductCatalog {
     
     
     /**
-     * Fill the Map with all products through a query.
+     * Takes a query from the database, returning all the the products in the database and puts them into the TreeMap "products"
      */
     private void fillProducts() {
-        DBConnection dbCon = new DBConnection();
-        String query = "SELECT * FROM products";
-        ResultSet rs = dbCon.runQueryExcecute(query);
+        DBConnection dbCon = new DBConnection(); //Creates an instance of the databasemanager
+        String query = "SELECT * FROM products"; //The query, which the ResultSet will use
+        ResultSet rs = dbCon.runQueryExcecute(query); //The ResultSet then uses the query to run
 
         try{
-            while(rs.next()){
+            while(rs.next()){ //Iterates through the ResultSet and matches the variables below with the column attributes.
                 long productNum = rs.getInt("product_id");              
                 String productName = rs.getString("name");
                 double price = rs.getDouble("price");
                 String description = rs.getString("description");
                 String productCategory = rs.getString("category");
                 
-                this.products.put(productNum, new Product(productName, productNum, price, description, ProductCategory.valueOf(productCategory)));
+                if(Arrays.asList(ProductCategory.values()).contains(productCategory)){
+                    this.products.put(productNum, new Product(productName, productNum, price, description, ProductCategory.valueOf(productCategory))); //Adds it to the TreeMap
+                }else {
+                    System.err.println("'"+productCategory+"" + "is not found in the database");
+                }
+                
             }
         } catch (SQLException exception){
             exception.printStackTrace();
@@ -56,7 +62,11 @@ public class ProductCatalog {
             
         
     }
-    
+    /**
+     * Iterates through the list of products, checks if any of the attributes of the products in that list match with the search text and adds these to a new arrayList. The list is then returned.
+     * @param in Takes the search text as an input in the form of a String
+     * @return returns a list of products matching the search text
+     */
     public ArrayList<Product> SearchProduct(String in){
         this.searchProducts = new ArrayList();
         
@@ -87,7 +97,7 @@ public class ProductCatalog {
     }
     
     /**
-     * Used when
+     * Used when?
      * @param prdCat
      * @return 
      */
